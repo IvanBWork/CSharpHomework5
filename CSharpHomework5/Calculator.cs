cf7f50d0895517487ed76111b6c72583e268df2f
Ivan <IvanBWork@yandex.ru>
2024-08-22 01:42:47 +0300

    Добавьте файлы проекта.

---
+++ CSharpHomework5/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomework5
+{
+    internal class Calculator : ICalculator
+    {
+        public event EventHandler<OperandChangedEventArgs> GotResult;
+        private Stack<double> stack = new Stack<double>();
+        private double Result { 
+            get 
+            {
+                return stack.Count() == 0 ? 0 : stack.Peek();
+            } 
+            set 
+            { 
+                stack.Push(value); 
+                RaiseEvent(); 
+            } 
+        }
+        public void RaiseEvent()
+        {
+            GotResult.Invoke(this, new OperandChangedEventArgs(Result));
+        }
+        public void CancelLast() 
+        {
+            if (stack.Count > 0)
+            {
+                stack.Pop();
+                RaiseEvent();
+            }
+        }
+        public void Divide(double number)
+        {
+             Result /= number;
+        }
+
+        public void Multiply(double number)
+        {
+             Result *= number;
+        }
+
+        public void Substract(double number)
+        {
+             Result -= number;
+        }
+
+        public void Sum(double number)
+        {
+             Result += number;
+        }
+        
+    }
+}
---
+++ CSharpHomework5/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace CSharpHomework5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 0;
+            string? action = "";
+            
+
+            ICalculator calculator = new Calculator();
+            calculator.GotResult += Calculator_GotResult;
+
+            while (number != null)
+            {
+                Console.Write("Введите число: ");                
+                string? input = Console.ReadLine();
+                if (input.Equals("q") || input.Equals("")) return;
+                
+                number = ReadDouble(input);
+
+                Console.Write("Введите арифметическое действие: ");
+                action = Console.ReadLine();
+                switch (action)
+                {
+                    case "+":
+                        calculator.Sum(number);
+                        break;
+                    case "-":
+                        calculator.Substract(number);
+                        break;
+                    case "*":
+                        calculator.Multiply(number);
+                        break;
+                    case "/":
+                        calculator.Divide(number);
+                        break;
+                    case "q":
+                        return;                           
+                    case "":
+                        return;
+                    default:
+                        Console.WriteLine("Вы ввели неверное действие.");
+                        Console.WriteLine();
+                        break;
+                }                               
+            }
+        }
+
+        private static void Calculator_GotResult(object? sender, OperandChangedEventArgs e)
+        {
+            Console.WriteLine($"Итог: {e.Operand}");
+            Console.WriteLine();
+        }
+
+        private static double ReadDouble(string? input)
+        {            
+            double i;
+            while (!double.TryParse(input, out i))
+            {                
+                Console.WriteLine("Вы ввели не число!");
+                input = Console.ReadLine();                
+            }
+            return i;
+        }
+    }
+}
